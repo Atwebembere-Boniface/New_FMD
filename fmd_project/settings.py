f96128ad8b1f726da511aa98042dc8dd45f3de1b
Atwebembere-Boniface <atwebembereboniface@gmail.com>
2026-02-22 18:54:29 +0300

    Temporary superuser creation

--- fmd_project/settings.py
+++ fmd_project/settings.py
@@ -256,9 +256,5 @@ LOGGING = {
 
 
 
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@email.com", "admin123")
